b68af426799f0f48ca754b5bcc6c9df1f1c1d6e2
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-03-27 00:44:05 +0000

    feat: add dummy send_email/1

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -2,4 +2,10 @@ defmodule Sender do
   @moduledoc """
   Documentation for `Sender`.
   """
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email sent"}
+  end
 end
