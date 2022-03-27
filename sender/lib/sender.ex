9f63b559e02e7ec3179d57a3008e5b3772bcbf4b
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-03-27 00:41:05 +0000

    chore: remove hello/0

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -2,17 +2,4 @@ defmodule Sender do
   @moduledoc """
   Documentation for `Sender`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Sender.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
