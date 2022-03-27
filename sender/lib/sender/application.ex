c7189750b0a1fd135e4f7f8a07471f8ae439af2d
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-03-27 00:39:19 +0000

    doc: remove comments from application

--- sender/lib/sender/application.ex
+++ sender/lib/sender/application.ex
@@ -1,6 +1,4 @@
 defmodule Sender.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,8 @@ defmodule Sender.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Sender.Worker.start_link(arg)
-      # {Sender.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Sender.Supervisor]
     Supervisor.start_link(children, opts)
   end
