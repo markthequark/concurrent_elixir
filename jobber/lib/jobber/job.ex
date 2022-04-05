d3e9b17d42d65cf44e0c0363b9377373c68d5bd6
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-04-05 13:15:42 +0100

    use registry for job ids

--- jobber/lib/jobber/job.ex
+++ jobber/lib/jobber/job.ex
@@ -5,12 +5,22 @@ defmodule Jobber.Job do
   defstruct [:work, :id, :max_retries, retries: 0, status: "new"]
 
   def start_link(args) do
-    GenServer.start_link(__MODULE__, args)
+    args =
+      if Keyword.has_key?(args, :id) do
+        args
+      else
+        Keyword.put(args, :id, random_job_id())
+      end
+
+    id = Keyword.get(args, :id)
+    type = Keyword.get(args, :type)
+
+    GenServer.start_link(__MODULE__, args, name: via(id, type))
   end
 
   def init(args) do
     work = Keyword.fetch!(args, :work)
-    id = Keyword.get(args, :id, random_job_id())
+    id = Keyword.get(args, :id)
     max_retries = Keyword.get(args, :max_retries, 3)
 
     state = %Jobber.Job{id: id, work: work, max_retries: max_retries}
@@ -57,5 +67,9 @@ defmodule Jobber.Job do
 
   defp random_job_id() do
     :crypto.strong_rand_bytes(5) |> Base.url_encode64(padding: false)
+  end
+
+  defp via(key, value) do
+    {:via, Registry, {Jobber.JobRegistry, key, value}}
   end
 end
