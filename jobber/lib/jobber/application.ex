2f390e6658092b6fbbff9d459ee93d914a0a008f
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-04-05 01:50:10 +0100

    add dynamic supervisor

--- jobber/lib/jobber/application.ex
+++ jobber/lib/jobber/application.ex
@@ -5,15 +5,11 @@ defmodule Jobber.Application do
 
   use Application
 
-  @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Jobber.Worker.start_link(arg)
-      # {Jobber.Worker, arg}
+      {DynamicSupervisor, strategy: :one_for_one, name: Jobber.JobRunner},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Jobber.Supervisor]
     Supervisor.start_link(children, opts)
   end
