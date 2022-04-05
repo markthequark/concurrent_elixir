27fb748d6ff4054496388566139d36f13ed0f385
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-04-05 23:57:50 +0100

    add dummy work function

--- scraper/lib/scraper.ex
+++ scraper/lib/scraper.ex
@@ -1,18 +1,15 @@
 defmodule Scraper do
   @moduledoc """
-  Documentation for `Scraper`.
+  Documentation for Scraper.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Scraper.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def work() do
+    # For simplicity, this function is
+    # just a placeholder and does not contain
+    # real scraping logic.
+    1..5
+    |> Enum.random()
+    |> :timer.seconds()
+    |> Process.sleep()
   end
 end
