60a05531e0af6fedfd65ee076bcef1b0b53f9566
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-04-05 01:29:52 +0100

    send emails in single proceses genserver with retries

--- sender/lib/send_server.ex
+++ sender/lib/send_server.ex
@@ -1,5 +1,6 @@
 defmodule SendServer do
   use GenServer
+  require Logger
 
   def start_link(state, opts) do
     GenServer.start_link(__MODULE__, state, opts)
@@ -8,6 +9,8 @@ defmodule SendServer do
   def init(opts) do
     max_retries = Keyword.get(opts, :max_retries, 5)
     state = %{emails: [], max_retries: max_retries}
+
+    schedule_retry()
     {:ok, state}
   end
 
@@ -16,12 +19,45 @@ defmodule SendServer do
   end
 
   def handle_cast({:send, email}, state) do
-    Sender.send_email(email)
-    emails = [%{email: email, status: "sent", retries: 0} | state.emails]
+    status = send_email(email)
+
+    emails = [%{email: email, status: status, retries: 0} | state.emails]
     {:noreply, %{state | emails: emails}}
   end
 
   def handle_cast(_msg, state) do
     {:noreply, state}
+  end
+
+  def handle_info(:retry, state) do
+    emails =
+      for email <- state.emails do
+        if email.status == "failed" and email.retries < state.max_retries do
+          Logger.info("Retrying email #{email.email}...")
+
+          status = send_email(email.email)
+          %{email | status: status, retries: email.retries + 1}
+        else
+          email
+        end
+      end
+
+    schedule_retry()
+    {:noreply, %{state | emails: emails}}
+  end
+
+  def terminate(reason, _state) do
+    IO.puts("Terminating with reason #{reason}")
+  end
+
+  defp send_email(email) do
+    case Sender.send_email(email) do
+      {:ok, "email_sent"} -> "sent"
+      :error -> "failed"
+    end
+  end
+
+  defp schedule_retry(time \\ :timer.seconds(5)) do
+    Process.send_after(self(), :retry, time)
   end
 end
