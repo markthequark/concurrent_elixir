6064b56332b9f236704dd16ee3a950a8218bea2a
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-04-07 04:37:57 +0100

    flow reducers

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -8,17 +8,22 @@ defmodule Airports do
   def open_airports() do
     airports_csv()
     |> File.stream!()
-    |> CSV.parse_stream()
     |> Flow.from_enumerable()
     |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
       %{
-        id: :binary.copy(Enum.at(row, 0)),
-        type: :binary.copy(Enum.at(row, 2)),
-        name: :binary.copy(Enum.at(row, 3)),
-        country: :binary.copy(Enum.at(row, 8))
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.reduce(fn -> %{} end, fn item, acc ->
+      Map.update(acc, item.country, 1, &(&1 + 1))
+    end)
     |> Enum.to_list()
   end
 
@@ -37,6 +42,36 @@ defmodule Airports do
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.group_by(& &1.country)
+    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
     |> Enum.to_list()
+  end
+
+  def open_airports() do
+    airports_csv()
+    |> File.stream!()
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0, 0, 0, 1]))
+      event
+    end)
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.group_by(& &1.country)
+    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
+    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
+    |> Enum.to_list()
+    |> List.flatten()
   end
 end
