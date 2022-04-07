f346b4539fbc101adb1614e54866664326ea9718
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2022-04-07 02:56:30 +0100

    csv file and parser

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -1,18 +1,22 @@
 defmodule Airports do
-  @moduledoc """
-  Documentation for `Airports`.
-  """
+  alias NimbleCSV.RFC4180, as: CSV
 
-  @doc """
-  Hello world.
+  def airports_csv() do
+    Application.app_dir(:airports, "/priv/airports.csv")
+  end
 
-  ## Examples
-
-      iex> Airports.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def open_airports() do
+    airports_csv()
+    |> File.read!()
+    |> CSV.parse_string()
+    |> Enum.map(fn row ->
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Enum.reject(&(&1.type == "closed"))
   end
 end
